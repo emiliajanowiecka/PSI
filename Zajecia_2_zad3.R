bc81e13ea9c8da1cec9ce7ab0f59c84d429c814f
emiliajanowiecka <e.janowiecka@student.uw.edu.pl>
2025-03-03 23:33:50 +0100

    Add files via upload

---
+++ Zajecia_2_zad3.R
@@ -0,0 +1,52 @@
+#Zadanie 3
+
+# Wczytaj dane tekstowe
+# Wczytaj plik tekstowy z lokalnego dysku
+text <- readLines(file.choose())
+text
+
+install.packages("qdap")
+library(qdap)
+
+frequent_terms <- freq_terms(text)
+frequent_terms
+frequent_terms <- freq_terms(text, stopwords = Top200Words)
+plot(frequent_terms)
+
+# Tworzenie chmury słów za pomocą pakietu wordcloud
+install.packages("wordcloud")
+library(wordcloud)
+
+# Utwórz chmurę słów
+wordcloud(frequent_terms$WORD, frequent_terms$FREQ)
+
+# Opcje chmury słów
+?wordcloud
+# Zmiana wartości min.freq i max.words w celu wyświetlenia mniejszej/większej liczby słów.
+# min.freq: słowa o częstości poniżej tej wartości nie będą wyświetlane
+# max.words: maksymalna liczba słów do wyświetlenia
+
+# Ograniczenie liczby słów w chmurze poprzez określenie minimalnej częstości
+wordcloud(frequent_terms$WORD, frequent_terms$FREQ, min.freq = 4)
+
+# Ograniczenie liczby słów w chmurze poprzez określenie maksymalnej liczby słów
+wordcloud(frequent_terms$WORD, frequent_terms$FREQ, max.words = 5)
+
+# Dodanie różnych palet kolorystycznych
+wordcloud(frequent_terms$WORD, frequent_terms$FREQ, min.freq = 4, colors = brewer.pal(9,"Blues"))
+wordcloud(frequent_terms$WORD, frequent_terms$FREQ, min.freq = 4, colors = brewer.pal(9,"Reds"))
+wordcloud(frequent_terms$WORD, frequent_terms$FREQ, min.freq = 4, colors = brewer.pal(9,"Greens"))
+
+# Optymalizacja i dostosowanie wyników
+# Dodanie koloru do chmury słów dla lepszej wizualizacji
+# Dodanie koloru
+wordcloud(frequent_terms$WORD, frequent_terms$FREQ, min.freq = 4, colors = brewer.pal(8,"Dark2"))
+# Dodanie koloru
+wordcloud(frequent_terms$WORD, frequent_terms$FREQ, max.words = 5, colors = brewer.pal(8,"Accent"))
+?brewer.pal
+brewer.pal.info
+
+# W przemówieniu z 2021 dominują słowa związane z rodziną i zatrudnieniem, takie jak „jobs”, „families”, „care” i „child”.
+# Natomiast w 2024 roku częściej pojawiają się terminy dotyczące finansów i przyszłości, m.in. „pay”, „tax”, „bill” oraz „million”.
+# To sugeruje zmianę priorytetów na przestrzeni lat.
+
